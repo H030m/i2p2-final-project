78cab6674b3cd2359ca0d37e3cfbb4b4b538b90d
whale_island~ <a0905217134@gmail.com>
2025-05-11 00:00:34 +0800

    finish scoreboard and summit

---
+++ 2025_I2P2_TowerDefense-main/Engine/IScene.hpp
@@ -0,0 +1,81 @@
+#ifndef ISCENE_HPP
+#define ISCENE_HPP
+
+#include "Group.hpp"
+#include <ctime>
+#include <iostream>
+namespace Engine {
+    /// <summary>
+    /// This class should be inherited whenever you want to define a new scene.
+    /// Responsible for delegating draw, update, events to the objects and controls in scene.
+    /// Acts like a prototype, so all initialize and terminate shouldn't be put in constructor or destructor.
+    /// </summary>
+    class IScene : public Group {
+    protected:
+        /// <summary>
+        /// The interface cannot be instantiated directly, must be inherited.
+        /// </summary>
+        explicit IScene() = default;
+
+    public:
+        /// <summary>
+        /// Copy constructor is deleted, no copying allowed.
+        /// </summary>
+        IScene(IScene const &) = delete;
+        /// <summary>
+        /// Copy assignment operator is deleted, no copy assignment allowed.
+        /// </summary>
+        IScene &operator=(IScene const &) = delete;
+        /// <summary>
+        /// The default virtual destructor to support polymorphism destruction.
+        /// </summary>
+        virtual ~IScene() = default;
+        /// <summary>
+        /// Put all initialize code here instead of constructor to make change scene more simple.
+        /// </summary>
+        virtual void Initialize() = 0;
+        /// <summary>
+        /// Put all terminate code here instead of destructor to make change scene more simple.
+        /// </summary>
+        virtual void Terminate();
+        /// <summary>
+        /// Draw to window display.
+        /// This is called when the game should redraw the window.
+        /// Delegate the draw event to all visible objects.
+        /// </summary>
+        void Draw() const override;
+    };
+}
+// TODO-scoreboard
+class PlayerInformation{
+public:
+    std::string name;
+    int score;
+    std::time_t Time;
+    int gap;
+    PlayerInformation(std::string name, int score, std::time_t Time, int gap):name(name), score(score), Time(Time), gap(gap){}
+    PlayerInformation():name("notfound"), score(0), Time(0), gap(0){}
+    ~PlayerInformation() = default;
+    bool operator()(const PlayerInformation & rhs) const{
+        if(score == rhs.score)
+            if(gap == rhs.gap)
+                return Time < rhs.Time;
+            else 
+                return gap < rhs.gap;
+        else return score < rhs.score;
+    }
+    bool operator>(const PlayerInformation & rhs) const{
+        if(score == rhs.score)
+            if(gap == rhs.gap)
+                return Time > rhs.Time;
+            else 
+                return gap > rhs.gap;
+        else return score > rhs.score;
+    }
+    std::string TimeToString_Time() {
+        char buf[100];
+        std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&Time));
+        return std::string(buf);
+    }
+};  
+#endif   // ISCENE_HPP
---
+++ 2025_I2P2_TowerDefense-main/Scene/SubmitScene.cpp
@@ -0,0 +1,90 @@
+#include <functional>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include "Engine/AudioHelper.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Engine/Point.hpp"
+#include "PlayScene.hpp"
+#include "UI/Component/Image.hpp"
+#include "UI/Component/ImageButton.hpp"
+#include "UI/Component/Label.hpp"
+#include "SubmitScene.hpp"
+
+
+void SubmitScene::Initialize() {
+    // grade
+    CulGrade();
+
+    int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
+    int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
+    int halfW = w / 2;
+    int halfH = h / 2;
+    
+    // Cancel Button
+    Engine::ImageButton *btn;
+    btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW - 450, halfH * 7 / 4 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&SubmitScene::BackOnClick, this, 2));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Cancel", "pirulen.ttf", 48, halfW - 250, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
+    
+    //switch to submit Scene
+    btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW + 50, halfH * 7 / 4 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&SubmitScene::SubmitOnClick, this, 2));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Submit", "pirulen.ttf", 48, halfW + 250, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
+
+    // score label
+    AddNewObject(new Engine::Label("SCORE: " + std::to_string(score), "pirulen.ttf", 64, halfW, 170, 255, 215, 0, 255, 0.5, 0.5));
+
+    // input your name
+    AddNewObject(new Engine::Label("Your Name", "pirulen.ttf", 64, halfW, 350, 255, 255, 255, 255, 0.5, 0.5));
+    NameLabel = new Engine::Label("", "pirulen.ttf", 40, halfW, 450, 255, 255, 255, 255, 0.5, 0.5);
+    AddNewObject(NameLabel);
+}
+void SubmitScene::Terminate() {
+    IScene::Terminate();
+}
+void SubmitScene::Update(float deltaTime) {
+    ticks += deltaTime;
+    if (ticks > 4 && ticks < 100 &&
+        dynamic_cast<PlayScene *>(Engine::GameEngine::GetInstance().GetScene("play"))->MapId == 2) {
+        ticks = 100;
+        bgmId = AudioHelper::PlayBGM("happy.ogg");
+    }
+}
+void SubmitScene::BackOnClick(int stage) {
+    // Change to select scene.
+    Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+}
+void SubmitScene::SubmitOnClick(int stage){
+    std::string filename = "../Resource/scoreboard.txt";
+    std::ifstream fin(filename);
+    std::vector<std::string>files;
+    std::string buffer;
+    while(getline(fin, buffer))files.push_back(buffer);
+    fin.close();
+    std::ofstream fout(filename);
+    for(auto it:files)fout<<it<<'\n';
+    fout<<name + " " + std::to_string(score) + std::to_string(EndTime) + std::to_string(StartTime - EndTime)<<'\n';
+    SubmitScene::BackOnClick(1);
+}
+void SubmitScene::CulGrade(){
+    if(MapId == 1){
+        score = ((StartTime - EndTime) + 100) + money / 10 + lives*10;
+    }else{
+        score = 0;
+    }
+}
+
+void SubmitScene::OnKeyDown(int keyCode) {
+    IScene::OnKeyDown(keyCode);
+    std::cerr<<keyCode<<" olasdff\n";
+    if(keyCode == 63){
+        name.pop_back();
+    }else if(ALLEGRO_KEY_A <= keyCode && keyCode <= ALLEGRO_KEY_Z){
+        name.push_back((char)(keyCode - ALLEGRO_KEY_A + 'A'));
+    }
+    NameLabel->Text = name;
+}
